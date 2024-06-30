1009e07a22702ce7488a52c26f8fee3e91c67561
Sachinth Yasasween Hewa Viththarachchige <117598245+sachinthyh@users.noreply.github.com>
2024-06-30 09:54:14 -0500

    LHS of p_eqn and q_eqn fixed.

--- se_opf_model.py
+++ se_opf_model.py
@@ -2,6 +2,7 @@
 # importing necessary libraries and packages
 import pyomo
 import pyomo.environ as pe
+import numpy as np
 
 model = pe.AbstractModel(name="seopf")
 
@@ -72,37 +73,32 @@ model.obj_seopf = pe.Objective(rule=obj_seopf_rule)
 
 # Constraints
 # Power Flow Equations
-
-# Original
-def p_eqn_rule(model, i):
-    gen_sum = sum(model.p_gen[i, g]/100
-                  if (i,g) in model.G
-                  else 0 for (i,g) in model.G)
-    aggr_sum = sum(-(model.p_a[i, a]/100)
-                   if (i,a) in model.A
-                   else 0 for (i,a) in model.A)
-    left_sum = gen_sum + aggr_sum
-    right_sum = sum(model.v[i]*model.v[j]*(model.gg[i, j]*pe.cos(model.t[i] - model.t[j])
+def p_eqn_rule(model, i):  # Fixed
+    left_sum = sum(model.p_gen[b, g]/100
+                  for (b,g) in model.G
+                  if (b == i))
+    left_sum += sum(-(model.p_a[b, a]/100)
+                   for (b,a) in model.A
+                   if (b == i))
+    right_sum = model.v[i]*sum(model.v[j]*(model.gg[i, j]*pe.cos(model.t[i] - model.t[j])
                                           + model.bb[i, j]*pe.sin(model.t[i] - model.t[j]))
                                if ((i < j) and ((i,j) in model.Y))
                                else model.v[j]*(model.gg[j, i]*pe.cos(model.t[i] - model.t[j])
                                           + model.bb[j, i]*pe.sin(model.t[i] - model.t[j]))
-                    if ((i >= j) and ((j,i) in model.Y))
+                    if ((i > j) and ((j,i) in model.Y))
     else 0
                                for i in model.B for j in model.B)
-    return (left_sum == right_sum)
+    return left_sum == right_sum
 model.p_eqn = pe.Constraint(model.B, rule=p_eqn_rule)
 
-
 def q_eqn_rule(model, i):
-    gen_sum = sum(model.q_gen[i, g]/100
-                  if (i,g) in model.G
-                  else 0 for (i,g) in model.G)
-    aggr_sum = sum(-(model.q_a[i, a]/100)
-                   if (i,a) in model.A
-                   else 0 for (i,a) in model.A)
-    left_sum = gen_sum + aggr_sum
-    right_sum = sum(model.v[i]*model.v[j]*(model.gg[i, j]*pe.sin(model.t[i] - model.t[j])
+    left_sum = sum(model.q_gen[b, g]/100
+                   for (b,g) in model.G
+                   if (b == i))
+    left_sum += sum(-(model.q_a[b, a]/100)
+                   for (b,a) in model.A
+                   if (b == i))
+    right_sum = model.v[i]*sum(model.v[j]*(model.gg[i, j]*pe.sin(model.t[i] - model.t[j])
                                           - model.bb[i, j]*pe.cos(model.t[i] - model.t[j]))
                                if ((i < j) and ((i,j) in model.Y))
                                else model.v[j]*(model.gg[j, i]*pe.sin(model.t[i] - model.t[j])
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # Power Flow Equation Fix
+'''
 def p_eqn_rule(model, i):  # Fixed
     gen_sum = sum(model.p_gen[i, g]/100
                   if (i,g) in model.G
@@ -28,6 +29,83 @@ def q_eqn_rule(model, i):  # Fixed
                    else 0 for (i,a) in model.A)
     left_sum = gen_sum + aggr_sum
     right_sum = sum(model.v[i]*model.v[j]*(model.gg[i, j]*pe.sin(model.t[i] - model.t[j])
+                                          - model.bb[i, j]*pe.cos(model.t[i] - model.t[j]))
+                               if ((i < j) and ((i,j) in model.Y))
+                               else model.v[j]*(model.gg[j, i]*pe.sin(model.t[i] - model.t[j])
+                                          - model.bb[j, i]*pe.cos(model.t[i] - model.t[j]))
+                    if ((i > j) and ((j,i) in model.Y))
+    else 0
+                               for i in model.B for j in model.B)
+    return (left_sum == right_sum)
+model.q_eqn = pe.Constraint(model.B, rule=q_eqn_rule)
+'''
+
+def p_eqn_rule(model, i):
+    return (sum(model.p_gen[i, g]/100
+                  if (i,g) in model.G
+                  else 0
+               - (model.p_a[i, a] / 100)
+               if (i, a) in model.A
+    else 0
+               for (i, g) in model.G
+               for (i, a) in model.A
+               ) ==
+            (model.v[i]*sum(model.v[j]*(model.gg[i, j]*pe.cos(model.t[i] - model.t[j])
+                                          + model.bb[i, j]*pe.sin(model.t[i] - model.t[j]))
+                               if ((i < j) and ((i,j) in model.Y))
+                               else model.v[j]*(model.gg[j, i]*pe.cos(model.t[i] - model.t[j])
+                                          + model.bb[j, i]*pe.sin(model.t[i] - model.t[j]))
+                    if ((i >= j) and ((j,i) in model.Y))
+    else 0
+                               for i in model.B for j in model.B)))
+model.p_eqn = pe.Constraint(model.B, rule=p_eqn_rule)
+
+
+def q_eqn_rule(model, i):
+    left_sum = sum(model.q_gen[i, g]/100
+                  if (i,g) in model.G
+                  else 0 for (i,g) in model.G)
+    left_sum += sum(-(model.q_a[i, a]/100)
+                   if (i,a) in model.A
+                   else 0 for (i,a) in model.A)
+    right_sum = model.v[i]*sum(model.v[j]*(model.gg[i, j]*pe.sin(model.t[i] - model.t[j])
+                                          - model.bb[i, j]*pe.cos(model.t[i] - model.t[j]))
+                               if ((i < j) and ((i,j) in model.Y))
+                               else model.v[j]*(model.gg[j, i]*pe.sin(model.t[i] - model.t[j])
+                                          - model.bb[j, i]*pe.cos(model.t[i] - model.t[j]))
+                    if ((i > j) and ((j,i) in model.Y))
+    else 0
+                               for i in model.B for j in model.B)
+    return (left_sum == right_sum)
+model.q_eqn = pe.Constraint(model.B, rule=q_eqn_rule)
+
+# LATEST
+def p_eqn_rule(model, i):  # Fixed
+    left_sum = sum(model.p_gen[b, g]/100
+                  for (b,g) in model.G
+                  if (b == i))
+    left_sum += sum(-(model.p_a[b, a]/100)
+                   for (b,a) in model.A
+                   if (b == i))
+    right_sum = sum(model.v[i]*model.v[j]*(model.gg[i, j]*pe.cos(model.t[i] - model.t[j])
+                                          + model.bb[i, j]*pe.sin(model.t[i] - model.t[j]))
+                               if ((i < j) and ((i,j) in model.Y))
+                               else model.v[j]*(model.gg[j, i]*pe.cos(model.t[i] - model.t[j])
+                                          + model.bb[j, i]*pe.sin(model.t[i] - model.t[j]))
+                    if ((i > j) and ((j,i) in model.Y))
+    else 0
+                               for i in model.B for j in model.B)
+    return left_sum == right_sum
+model.p_eqn = pe.Constraint(model.B, rule=p_eqn_rule)
+
+def q_eqn_rule(model, i):
+    left_sum = sum(model.q_gen[b, g]/100
+                   for (b,g) in model.G
+                   if (b == i))
+    left_sum += sum(-(model.q_a[b, a]/100)
+                   for (b,a) in model.A
+                   if (b == i))
+    right_sum = model.v[i]*sum(model.v[j]*(model.gg[i, j]*pe.sin(model.t[i] - model.t[j])
                                           - model.bb[i, j]*pe.cos(model.t[i] - model.t[j]))
                                if ((i < j) and ((i,j) in model.Y))
                                else model.v[j]*(model.gg[j, i]*pe.sin(model.t[i] - model.t[j])
