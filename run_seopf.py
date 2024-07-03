4730d688ab2a7e16f4b52d4479b332a767c36967
Sachinth Yasasween Hewa Viththarachchige <117598245+sachinthyh@users.noreply.github.com>
2024-07-03 07:13:51 -0500

    Fixed all fundamentals errors.

--- run_seopf.py
+++ run_seopf.py
@@ -9,19 +9,14 @@ import pyomo.environ as pe
 model = se.model
 data = di.instance_data
 
-print(data[None]['GB'][None])
-print(data[None]['vg'])
-
 model_instance = se.create_pyomo_instance(model, data)
 
-def run_opf(model_instance, data):
-    for bus in data[None]['GB'][None]:  # Fixing the voltage magnitudes at generator buses (slack or PV buses)
-        model_instance.v[bus].fix(data[None]['vg'][bus])
+def run_opf(model_instance):
     model_instance.t[13].fix(0)  # Fixing the angle of slack bus, selecting bus 13 as the slack bus
     opt = pe.SolverFactory('ipopt')
     opt.solve(model_instance)
 
-model_instance.pprint()
+run_opf(model_instance)
 
 
-run_opf(model_instance, data)
+# run_opf(model_instance)
