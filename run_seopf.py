bfc3ed0f3af856c4b60c13545e498676c51a9e12
Sachinth Yasasween Hewa Viththarachchige <117598245+sachinthyh@users.noreply.github.com>
2024-07-07 23:09:02 -0500

    Refactored files for main.py

--- run_seopf.py
+++ run_seopf.py
@@ -1,25 +1,7 @@
-'''
-Created for running the SEOPF model
-'''
-import dc_opf_model as dc
-import se_opf_model as se
-import data_instance as di
+'''Created for running the SEOPF model'''
+
 import pyomo.environ as pe
 from pyomo.opt import SolverFactory
-
-
-dc_model = dc.model
-se_model = se.model
-data_24 = di.instance_data_24
-data_5 = di.instance_data_5
-
-dc_instance = dc.create_pyomo_instance(dc_model, data_24)
-dc_instance.t[13].fix(0)  # Fixing the angle of slack bus, selecting bus 13 as the slack bus for 24-bus case
-
-se_instance = se.create_pyomo_instance(se_model, data_5)
-se_instance.t[1].fix(0)  # Fixing the angle of slack bus, selecting bus 1 as the slack bus for 5-bus case
-
-
 
 def run_opf(model_instance):
     solver_io = 'nl'
